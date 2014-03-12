cd3f58b554a67e9d573d8df25f207d7c6f5d8796
Nicolas Herment <nherment@Nicolass-MacBook-Pro.local>
2014-03-12 09:16:18 +0000

    + test argument passing

--- test/hapi.test.js
+++ test/hapi.test.js
@@ -16,13 +16,17 @@ si.add( {role:'ping'}, function(args, callback) {
   callback(null, 'pong')
 })
 
+si.add( {role:'hello'}, function(args, callback) {
+  callback(null, {result: 'hello ' + args.name })
+})
+
 
 var hapiPin = si.pin({role: 'hapi', cmd: '*'})
 
 
-describe('salestax', function () {
+describe('hapi', function () {
 
-  it('only required arguments', function (done) {
+  it('ping', function (done) {
     hapiPin.attach({
       name: 'test1',
       version: '0.0.1',
@@ -38,7 +42,39 @@ describe('salestax', function () {
         method: 'post',
         url: '/ping'
       }, function(res) {
+        assert.equal(res.statusCode, 200)
         assert.equal(res.payload, 'pong')
+        done()
+      })
+    })
+  })
+
+  it('POST body propagated to microservice', function (done) {
+    hapiPin.attach({
+      name: 'test2',
+      version: '0.0.1',
+      pack: server.pack,
+      connectors: [{
+          role: 'hello'
+        }
+      ]
+    }, function(err) {
+      assert.ok(!err)
+
+      var now = Date.now()
+
+      server.inject({
+        method: 'post',
+        url: '/hello',
+        payload: JSON.stringify({name: 'world ' + now})
+      }, function(res) {
+        assert.equal(res.statusCode, 200)
+        assert.ok(res.headers['content-type'])
+        assert.ok(res.headers['content-type'].indexOf('application/json') > -1)
+        assert.ok(res.payload)
+        var payload = JSON.parse(res.payload)
+        assert.ok(payload.result)
+        assert.equal(payload.result, 'hello world ' + now)
         done()
       })
     })
